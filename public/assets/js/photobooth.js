4da8c6efde4d70f85ecb82cee2575be20bf041fa
Jeffrey Shaw <jxjshaw@gmail.com>
2016-07-07 03:29:14 -0700

    Add cosmetic changes

--- public/assets/js/photobooth.js
+++ public/assets/js/photobooth.js
@@ -1,52 +1,30 @@
 
- $(document).ready(function() {
-
-   Webcam.set({
-                // live preview size
-                width: 640,
-                height: 480,
-                // device capture size
-                dest_width: 640,
-                dest_height: 480,
-                // final cropped size
-                crop_width: 480,
-                crop_height: 480,
-                // format and quality
-                image_format: 'jpeg',
-                jpeg_quality: 90,
-                // flip horizontal (mirror mode)
-                flip_horiz: true
-            });
-  Webcam.attach( '#my_camera' );
-
-  // click for random photo filter
-  $(document).click(function(){
-    var randomcolors = '#' + Math.floor(Math.random() * 16777215).toString(16); 
-    $('#polaroid-overlay').css("background-color", randomcolors);
+   //LAYOUT STUFF
+   $(document).ready(function() {
+    // Webcam Setup
+    Webcam.set({
+      width: 640,
+      height: 480,
+      dest_width: 640,
+      dest_height: 480,
+      crop_width: 480,
+      crop_height: 480,
+      image_format: 'jpeg',
+      jpeg_quality: 90,
+      flip_horiz: true
+    });
+    // Initialize Webcam
+    Webcam.attach('#my_camera');
+    // Randomize Filter On Click 
+    $(document).click(function(){
+      var randomcolors = '#' + Math.floor(Math.random() * 16777215).toString(16); 
+      $('#polaroid-overlay').css("background-color", randomcolors);
+    });
   });
- 
-  // import images from json file to film strip
-  // $("#owl-demo").owlCarousel({
-  //   jsonPath : '/assets/json/customData.json',
-  //   jsonSuccess : customDataSuccess
-  // });
- 
-  // function customDataSuccess(data){
-  //   var content = "";
-  //   for(var i in data["items"]){
-       
-  //      var img = data["items"][i].img;
-  //      var alt = data["items"][i].alt;
- 
-  //      content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
-  //   }
-  //   $("#owl-demo").html(content);
-  // }
-});
 
-  //Shuffle Gallery Function
+  //GALLERY SHUFFLE FUNCTION
   function shuffle_Pics(){
-     $('.items').each(function (index, element) {
+    $('.items').each(function (index, element) {
     var items = $(this).find('li img');
     items.each( function (index, element) {
       var item = $(this);
@@ -62,39 +40,58 @@
       CSS[TopBottom] = PosNeg+VertPos+'px';
       CSS[LeftRight] = PosNeg+HorPos+'px';
       item.css(CSS);
+      });
     });
-  });
   };
 
-  // Screencap Function
+  // SCREENCAP FUNCTION
   function take_snapshot() {
-
     Webcam.snap( function(data_uri) {
-      
-      
       //Add snapshot to list
       var newPic = '<img src="'+data_uri+'"/ class="captured">';
       $("ul").append("<li>"+newPic+"</li>");
       window.scrollTo(0,document.body.scrollHeight);
       shuffle_Pics();
-
-      // //Read from json
-      // var request = new XMLHttpRequest();
-      // request.open("GET", "/assets/json/customData.json", false);
-      // request.send(null)
-      // var my_JSON_object = JSON.parse(request.responseText);
-      // var picture = my_JSON_object["items"][1].img;
-      // alert (picture);
-      // $("ul").append("<li><img src="+picture+"></li>");
-      // $.getJSON('/assets/json/customData.json', function(data) {         
-      //     alert(data);
-      // });
-      // var words = data_uri;
-      // alert(words);
-      // document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/ class="captured">';
-                } );                    
+    });                    
   };
 
+
+    // Garbage~~~~~~~
+    // import images from json file to film strip
+    // $("#owl-demo").owlCarousel({
+    //   jsonPath : '/assets/json/customData.json',
+    //   jsonSuccess : customDataSuccess
+    // });
+   
+    // function customDataSuccess(data){
+    //   var content = "";
+    //   for(var i in data["items"]){
+         
+    //      var img = data["items"][i].img;
+    //      var alt = data["items"][i].alt;
+   
+    //      content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
+    //   }
+    //   $("#owl-demo").html(content);
+    // }
+
+
+    // Read from json stuff
+
+        // //Read from json
+        // var request = new XMLHttpRequest();
+        // request.open("GET", "/assets/json/customData.json", false);
+        // request.send(null)
+        // var my_JSON_object = JSON.parse(request.responseText);
+        // var picture = my_JSON_object["items"][1].img;
+        // alert (picture);
+        // $("ul").append("<li><img src="+picture+"></li>");
+        // $.getJSON('/assets/json/customData.json', function(data) {         
+        //     alert(data);
+        // });
+        // var words = data_uri;
+        // alert(words);
+        // document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/ class="captured">';
 
 
 
